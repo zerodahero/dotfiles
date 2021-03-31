e7e0e0aef767a1167e87836a3781f15e9b656401
Zack Teska <zack@kirschbaumdevelopment.com>
2021-03-31 08:47:27 -0500

    add yabai and skhd

--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ ln -si "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
 ln -si "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 ln -si "$HOME/dotfiles/.oh-my-zsh/custom/themes/agnoster.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/agnoster.zsh-theme"
 
-### OS Specific ZSHRC files
+### OS Specific  files
 case "$(uname -s)" in
     Darwin)
         ln -si "$HOME/dotfiles/.zshrc-darwin" "$HOME/.zshrc-darwin"
+        mkdir -p "$HOME/.config/yabai" "$HOME/.config/skhd"
+        ln -si "$HOME/dotfiles/yabai/yabairc" "$HOME/.config/yabai/yabairc"
+        ln -si "$HOME/dotfiles/skhd/skhdrc" "$HOME/.config/skhd/skhdrc"
     ;;
     Linux)
     ;;

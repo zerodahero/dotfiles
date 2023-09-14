28e549c8f4194598515c6eccbd62288fb64af6bc
Zack Teska <zack.teska@totalexpert.com>
2023-09-14 13:00:26 -0600

    fix startup

--- nvim/after/plugin/mini.lua
+++ nvim/after/plugin/mini.lua
@@ -3,11 +3,12 @@ require('mini.trailspace').setup()
 require('mini.splitjoin').setup()
 require('mini.misc').setup({})
 
-local starter = require('mini.starter')
-starter.setup({
-    evaluate_single = true,
-    items = {
-        starter.sections.builtin_actions(),
-        starter.sections.recent_files(10, true),
-    },
-})
+-- FUTURE: this is wonky with nvim-tree
+-- local starter = require('mini.starter')
+-- starter.setup({
+--     evaluate_single = true,
+--     items = {
+--         starter.sections.builtin_actions(),
+--         starter.sections.recent_files(10, true),
+--     },
+-- })

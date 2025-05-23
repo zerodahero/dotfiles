6555319e5aed9c071a9ce3b305aa5e81d6fbee9f
Zack Teska <zachary.teska@sayari.com>
2025-05-23 07:32:29 -0600

    fix: term info

--- zsh/plugins/environment/environment.plugin.zsh
+++ zsh/plugins/environment/environment.plugin.zsh
@@ -19,6 +19,6 @@ export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 export ANDROID_HOME="$HOME/Library/Android/sdk"
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
 
-# export TERM=xterm-256color
+export TERM=xterm-256color
 export PATH=$PATH:$(go env GOPATH)/bin:$(go env GOROOT)/bin:$HOME/.composer/vendor/bin:$HOME/bin:$HOME/.local/bin:$HOME/.nimble/bin:$HOME/.dotnet/tools:$HOME/.luarocks/bin
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
